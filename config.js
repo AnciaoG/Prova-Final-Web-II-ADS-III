83cb441565a5f99824a085df8cd949a7fb10cd50
AnciaoG <gabrielandrade@acad.ifma.edu.br>
2025-03-11 19:15:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('universidade', 'root', 'senha123', {
+const sequelize = new Sequelize('universidade', 'root', '', {
     host: 'localhost',
     dialect: 'mariadb'
 });
